e4900634379edb69b8ddc2f0513088e70f1848c8
Sheila-a <sheila.codes.m@gmail.com>
2023-05-24 12:42:32 +0100

    Elevate section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import UserJourney from "../components/UserJourney/UserJourney";
 import Footer from "../components/Footer/Footer";
 import Navbar from "../components/Navbar/Navbar";
+import Elevate from "../components/Elevate/Elevate";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
+      <Elevate />
       <UserJourney />
       <Footer />
     </div>
