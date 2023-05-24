99b7618c9cb1194d417cd084952f663636066f2a
kennyspecial <paulaekene@gmail.com>
2023-05-24 21:29:53 +0100

    hero

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import UserJourney from '../components/UserJourney/UserJourney'
 import Footer from  '../components/Footer/Footer'
+import Hero from  '../components/Hero/Hero'
 
 
 const Home = () => {
   return (
     <div>
+      <Hero/>
       <UserJourney/>
       <Footer/>
+      
     </div>
   );
 };
