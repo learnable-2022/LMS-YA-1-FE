6725c355e497be6bd6097399f0cea202c99178b6
Sheila-a <sheila.codes.m@gmail.com>
2023-06-11 20:12:41 +0100

    Fix hosting

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,35 +1,35 @@
-import { useState } from "react";
-import CarouselItem from "./CarouselItem";
-import example1Icon from "../../assets/carousel1.png";
-import example2Icon from "../../assets/carousel2.png";
-import example3Icon from "../../assets/carousel1.png";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
-import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import styles from "./Carousel.module.css";
+import { useState } from 'react';
+import CarouselItem from './CarouselItem';
+import example1Icon from '../../assets/Carousel1.png';
+import example2Icon from '../../assets/Carousel2.png';
+import example3Icon from '../../assets/Carousel1.png';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import styles from './Carousel.module.css';
 
 const Carousel = () => {
   const [activeIndex, setActiveIndex] = useState(0);
   const items = [
     {
       description:
-        "Lorem ipsum dolor sit amet consectetur. Sed commodo nibh etiam placerat. Ut eget in neque nulla vivamus. In proin arcu id risus volutpat rutrum eget purus odio.",
+        'Lorem ipsum dolor sit amet consectetur. Sed commodo nibh etiam placerat. Ut eget in neque nulla vivamus. In proin arcu id risus volutpat rutrum eget purus odio.',
       icon: example1Icon,
-      name: "Mantas",
-      title: "Learnable Intern",
+      name: 'Mantas',
+      title: 'Learnable Intern',
     },
     {
       description:
-        "Lorem ipsum dolor sit amet consectetur. Sed commodo nibh etiam placerat. Ut eget in neque nulla vivamus. In proin arcu id risus volutpat rutrum eget purus odio.",
+        'Lorem ipsum dolor sit amet consectetur. Sed commodo nibh etiam placerat. Ut eget in neque nulla vivamus. In proin arcu id risus volutpat rutrum eget purus odio.',
       icon: example2Icon,
-      name: "Mantas",
-      title: "Learnable Intern",
+      name: 'Mantas',
+      title: 'Learnable Intern',
     },
     {
       description:
-        "Lorem ipsum dolor sit amet consectetur. Sed commodo nibh etiam placerat. Ut eget in neque nulla vivamus. In proin arcu id risus volutpat rutrum eget purus odio.",
+        'Lorem ipsum dolor sit amet consectetur. Sed commodo nibh etiam placerat. Ut eget in neque nulla vivamus. In proin arcu id risus volutpat rutrum eget purus odio.',
       icon: example3Icon,
-      name: "Mantas",
-      title: "Learnable Intern",
+      name: 'Mantas',
+      title: 'Learnable Intern',
     },
   ];
 
@@ -45,18 +45,18 @@ const Carousel = () => {
 
   return (
     <div className={styles.main}>
-      <div className={styles["text-section"]}>
-        <h2 className={styles["text-title"]}>
+      <div className={styles['text-section']}>
+        <h2 className={styles['text-title']}>
           What our customers <br /> say about us
         </h2>
-        <p className={styles["text-description"]}>
+        <p className={styles['text-description']}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo
           nibh etiam placerat. Ut eget in neque nulla vivamus. In proin arcu id
           risus volutpat rutrum eget purus odio.
         </p>
-        <div className={styles["carousel-buttons"]}>
+        <div className={styles['carousel-buttons']}>
           <button
-            className={styles["carousel-arrow"]}
+            className={styles['carousel-arrow']}
             onClick={() => {
               updateIndex(activeIndex - 1);
             }}
@@ -64,7 +64,7 @@ const Carousel = () => {
             <ArrowBackIcon />
           </button>
           <button
-            className={styles["carousel-arrow"]}
+            className={styles['carousel-arrow']}
             onClick={() => {
               updateIndex(activeIndex + 1);
             }}
@@ -73,13 +73,13 @@ const Carousel = () => {
           </button>
         </div>
       </div>
-      <div className={styles["carousel"]}>
+      <div className={styles['carousel']}>
         <div
           className={styles.inner}
           style={{ transform: `translate(-${activeIndex * 100}%)` }}
         >
           {items.map((item, index) => (
-            <CarouselItem key={index} item={item} width={"100%"} />
+            <CarouselItem key={index} item={item} width={'100%'} />
           ))}
         </div>
       </div>
