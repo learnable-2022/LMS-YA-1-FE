3864ed4d487c64d87cd99b0e4ba6d9830b2ea36a
Sheila-a <sheila.codes.m@gmail.com>
2023-06-11 20:16:30 +0100

    Fix hosting

--- src/components/Modals/UploadImage/UploadImage.jsx
+++ src/components/Modals/UploadImage/UploadImage.jsx
@@ -1,10 +1,10 @@
-import { useState, useRef, useContext } from "react";
-import { useParams, useNavigate } from "react-router-dom";
-import AddButton from "../../AddButton/AddButton";
-import design from "./uploadImage.module.css";
-import Upload from "../../../assets/upload.png";
-import HighlightOffIcon from "@mui/icons-material/HighlightOff";
-import UserContext from "../../../context/UserContext";
+import { useState, useRef, useContext } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import AddButton from '../../AddButton/AddButton';
+import design from './uploadImage.module.css';
+import Upload from '../../../assets/upload.png';
+import HighlightOffIcon from '@mui/icons-material/HighlightOff';
+import UserContext from '../../../context/UserContext';
 
 const UploadImage = ({ handleShow, name }) => {
   const { setImageData } = useContext(UserContext);
@@ -21,11 +21,11 @@ const UploadImage = ({ handleShow, name }) => {
   const handleFileChange = (event) => {
     const file = event.target.files[0];
     if (file) {
-      if (file.type.startsWith("image/")) {
+      if (file.type.startsWith('image/')) {
         setSelectedFile(file);
-        console.log("Uploaded image:", file);
+        console.log('Uploaded image:', file);
       } else {
-        console.log("Please select an image file.");
+        console.log('Please select an image file.');
       }
     }
   };
@@ -35,7 +35,7 @@ const UploadImage = ({ handleShow, name }) => {
     setIsHovered(false);
     const file = event.dataTransfer.files[0];
     if (file) {
-      if (file.type.startsWith("image/")) {
+      if (file.type.startsWith('image/')) {
         setSelectedFile(file);
       }
     }
@@ -63,41 +63,41 @@ const UploadImage = ({ handleShow, name }) => {
     <div className={design.UploadVideo}>
       <div
         style={{
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "center",
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
         }}
       >
         <div></div>
         <HighlightOffIcon
           onClick={handleShow}
           style={{
-            cursor: "pointer",
-            color: "#292D32",
+            cursor: 'pointer',
+            color: '#292D32',
           }}
         />
       </div>
       <div
-        className={`${design.upload} ${isHovered ? design.uploadHovered : ""}`}
+        className={`${design.upload} ${isHovered ? design.uploadHovered : ''}`}
         onDrop={handleDrop}
         onDragOver={handleDragOver}
         onDragLeave={handleDragLeave}
       >
-        <img src={Upload} alt="" />
+        <img src={Upload} alt='' />
 
         <p>
           Drag & drop or <span onClick={handleFileSelect}>choose file</span> to
           upload Certificate.
           <input
-            type="file"
+            type='file'
             ref={fileInputRef}
-            style={{ display: "none" }}
-            accept="image/*"
+            style={{ display: 'none' }}
+            accept='image/*'
             onChange={handleFileChange}
           />
         </p>
-      </div>{" "}
-      <AddButton content="Save" onClick={handleUpload} />
+      </div>{' '}
+      <AddButton content='Save' onClick={handleUpload} />
     </div>
   );
 };
