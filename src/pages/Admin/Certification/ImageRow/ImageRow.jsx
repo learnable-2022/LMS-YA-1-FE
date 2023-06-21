f54fed87bc87415ce72207e70e553e11203363f9
Sheila-a <sheila.codes.m@gmail.com>
2023-06-21 15:24:24 +0100

    Uppercase

--- src/pages/Admin/Certification/ImageRow/ImageRow.jsx
+++ src/pages/Admin/Certification/ImageRow/ImageRow.jsx
@@ -1,8 +1,8 @@
-import styles from "./ImageRow.module.css";
-import { useParams, useNavigate } from "react-router-dom";
-import { useContext, useEffect } from "react";
-import ImageUploadPreview from "../../../../components/ImageUploadPreview/imageUploadPreview";
-import UserContext from "../../../../context/UserContext";
+import styles from './ImageRow.module.css';
+import { useParams, useNavigate } from 'react-router-dom';
+import { useContext, useEffect } from 'react';
+import ImageUploadPreview from '../../../../components/ImageUploadPreview/ImageUploadPreview';
+import UserContext from '../../../../context/UserContext';
 
 function ImageRow() {
   const { imageData } = useContext(UserContext);
@@ -14,22 +14,22 @@ function ImageRow() {
   }, [name]);
 
   return (
-    <div className={styles["videos-row"]}>
+    <div className={styles['videos-row']}>
       <h1>
         <span
-          style={{ fontWeight: "bold" }}
-          onClick={() => navigate("/certificate")}
+          style={{ fontWeight: 'bold' }}
+          onClick={() => navigate('/certificate')}
         >
           certificate
         </span>
       </h1>
 
-      <article className={styles["videos-container-title"]}>
-        <span className={styles["learning-material"]}>Image</span>
-        <span className={styles["file-name"]}>File name</span>
-        <span className={styles["date"]}>Date</span>
+      <article className={styles['videos-container-title']}>
+        <span className={styles['learning-material']}>Image</span>
+        <span className={styles['file-name']}>File name</span>
+        <span className={styles['date']}>Date</span>
       </article>
-      <section className={styles["videos-container"]}>
+      <section className={styles['videos-container']}>
         {imageData && (
           <ImageUploadPreview
             imagePrev={URL.createObjectURL(imageData.image)}
